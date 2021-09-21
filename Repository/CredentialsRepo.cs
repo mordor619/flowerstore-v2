fb84ecde9212b923fbb1799c0bc21218bea4d721
shrivalli04 <shrivalli04@gmail.com>
2021-09-21 11:10:54 +0530

    Add project files.

---
+++ Repository/CredentialsRepo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthorizationService.Repository
+{
+    public class CredentialsRepo: ICredentialsRepo
+    {
+        private Dictionary<string, string> ValidUsersDictionary = new Dictionary<string, string>()
+        {
+                {"user1","pass1"},
+               {"user2","pass2"},
+        };
+        public Dictionary<string,string> GetCredentials()
+        {
+            return ValidUsersDictionary;
+        }
+    }
+}
